01a2b759abe53c5ad31fbabf470e28755a5ccbe2
jlybianto <jesse.lybianto@gmail.com>
2016-03-07 21:36:43 -0700

    Initiate with package imported, data loaded and some profiling

---
+++ knn.py
@@ -0,0 +1,21 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+from sklearn import datasets
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+iris = datasets.load_iris()
+
+# ----------------
+# PROFILE DATA
+# ----------------
+
+observations = len(iris.data)
+features = len(iris.feature_names)
+print("Number of Observations: " + str(observations))
+print("Number of Features: " + str(features))
+print("")
